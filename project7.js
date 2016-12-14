ce1f3cb4722271f3e35cda86972c3b33675bd7ec
Jake Young <cheetopuffgaming@gmail.com>
2016-12-14 10:34:37 -0800

    did part 3,4,5

--- project7.js
+++ project7.js
@@ -1,6 +1,11 @@
 var screen = document.getElementById("screen")
 var namespace = "http://www.w3.org/2000/svg"
-
+var shouldDraw = false;
+//default rainbow color
+var rainbowColor = 0;
+//custom values
+var CustomColor = document.getElementById("CustomColor");
+var CustomSize = document.getElementById("CustomSize");
 // utility function
 function transformPoint(event) {
   var pt = screen.createSVGPoint()
@@ -11,41 +16,61 @@ function transformPoint(event) {
 }
 
 // Step 2: drawSquare and drawCircle functions
-function drawSquare(x, y, size, color) {
-  // square drawing code here
-  var newsquare = document.createElementNS(namespace, "rect")
-  newsquare.setAttribute("x", x)
-  newsquare.setAttribute("y", y)
-  newsquare.setAttribute("width", size)
-  newsquare.setAttribute("height", size)
-  newsquare.setAttribute("fill",color);
-  screen.appendChild(newsquare)
+function drawRectangle(color,width,x,y) {
+    var newrect = document.createElementNS(namespace, "rect")
+  newrect.setAttribute("x",   x)
+  newrect.setAttribute("y",   y)
+  newrect.setAttribute("width", width)
+  newrect.setAttribute("height", width)
+  newrect.setAttribute("fill", color)
+  screen.appendChild(newrect)
 }
 
-function drawCircle(cx, cy, radius, color) {
+function drawCircle(color,r,x,y) {
   var newcircle = document.createElementNS(namespace, "circle")
-  newcircle.setAttribute("cx", cx)
-  newcircle.setAttribute("cy", cy)
-  newcircle.setAttribute("r", radius)
+  newcircle.setAttribute("cx", x)
+  newcircle.setAttribute("cy", y)
   newcircle.setAttribute("fill", color)
-  screen.appendChild(newcircle)
+  newcircle.setAttribute("r", r)
+  screen.appendChild(newcircle);
+}
+function drawTriangle(color,x, y, base) {
+  var pts = "" + (x+ base*2) + "," + (y+ base*2) + " " + (x - base*2) + "," + (y+base*2) + " " + (x) + "," + (y - base*2)
+  var triangle = document.createElementNS(namespace, "polygon")
+  triangle.setAttribute("points", pts)
+  triangle.setAttribute("fill", color)
+  screen.appendChild(triangle)
 }
 
-var drawing = false
+function clearBoard(){
+screen.innerHTML = '';
+}
+
+function SetColor(){
+CustomColor.value = document.getElementById("CustomColorInput").value;
+}
+function SetSize(){
+CustomSize.value = document.getElementById("CustomSizeInput").value;
+}
 // Step 3: Event listeners
 document.addEventListener("mousedown", function(e) {
-  // what do you want to do when the user presses down
-  // on the mouse button?
-  drawing = true
-})
-
-document.addEventListener("mouseup", function(e) {
-  drawing = false
-})
-
-document.addEventListener("mousemove", function(e) {
-  if(drawing) {
-    var point = transformPoint(e)
-    drawCircle(point.x,point.y, 15,"red")
-  }
+    var pt = transformPoint(e, screen)
+    shouldDraw = true
+    var colorSelect = (document.getElementById("colorSelect").value)
+      var shapeSelect = ((document.getElementById("shapeSelect").value))
+          var sizeSelect = (document.getElementById("sizeSelect").value)
+      var pt = transformPoint(e, screen)
+      if(shouldDraw == true){
+        if(colorSelect == "rainbow"){
+          colorSelect = 'hsl('+(rainbowColor += 1)+', 100%, 50%)';
+        }
+      if(shapeSelect == "square"){
+    drawRectangle(colorSelect,sizeSelect,pt.x,pt.y);
+  }else if(shapeSelect == "circle"){
+    drawCircle(colorSelect,sizeSelect,pt.x,pt.y);
+      }
+      else{
+          drawTriangle(colorSelect,pt.x,pt.y,sizeSelect);
+      }
+    }
 })
